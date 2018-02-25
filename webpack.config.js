7797e5f6c9f4cf8307b5acf0bfb3c828e45c0868
Mark Probst <mark.probst@gmail.com>
2018-02-24 18:45:27 -0800

    Pack everything, uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
 module.exports = {
     entry: "./src/index",
     output: {
@@ -23,12 +25,18 @@ module.exports = {
         ]
     },
 
+    plugins: [
+        new UglifyJsPlugin()
+      ],
+    
     // When importing a module whose path matches one of the following, just
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
+    /*
     externals: {
         "react": "React",
         "react-dom": "ReactDOM"
     },
+    */
 };
